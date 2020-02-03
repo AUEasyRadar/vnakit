9338a1d8702ef1382c00b4cb956a4fb9d2d62a90
Tucker Johnston <btj0009@auburn.edu>
2020-02-03 14:50:19 -0600

    Added value validation to range and bandwidth

--- GUI/UI_Test.py
+++ GUI/UI_Test.py
@@ -25,7 +25,57 @@ def drawPlot(canvas, figure):
     canvasFigure.draw()
     canvasFigure.get_tk_widget().grid(column = 0, row = 0)
 
-def checkBounds()
+def checkBounds():
+    """Validates the input strings of the X limits boxes"""
+    lowerX = lowerXLimit.get()
+    if lowerX != '':
+        lowerX = int(float(lowerXLimit.get()))
+    upperX = upperXLimit.get()
+    if upperX != '':
+        upperX = int(float(upperXLimit.get()))
+    if upperX != '' and upperX > 6000:
+        upperX = 6000
+        upperXLimit.delete(0, 'end')
+        upperXLimit.insert(0,str(upperX))
+    elif lowerX != '' and lowerX < 100:
+        lowerX = 100
+        lowerXLimit.delete(0, 'end')
+        lowerXLimit.insert(0,str(lowerX))
+    if lowerX != '' and upperX != '' and upperX >= 100 and upperX < lowerX:
+        lowerX = upperX
+        lowerXLimit.delete(0, 'end')
+        lowerXLimit.insert(0,str(lowerX))
+    elif lowerX != '' and upperX != ''  and lowerX <= 6000 and upperX < lowerX:
+        upperX = lowerX
+        upperXLimit.delete(0, 'end')
+        upperXLimit.insert(0,str(upperX))
+    checkResolutionBandwidth()
+    return 1
+def checkResolutionBandwidth():
+    """Validates the input divides evenly into the x-limits"""
+    lowerX = lowerXLimit.get()
+    if lowerX != '':
+        lowerX = int(lowerX)
+    upperX = upperXLimit.get()
+    if upperX != '':
+        upperX = int(upperX)
+    rbw = resolutionBandwidth.get()
+    if rbw != '':
+        rbw = float(rbw)
+    if  lowerX != '' and upperX != '' and rbw != '':
+        difference = float(upperX - lowerX)
+        result = difference % rbw
+        if result != 0:
+            newValue = difference / rbw
+            if (newValue - int(newValue)) >= 0.5:
+                newValue = float(difference / (int(newValue) + 1))
+            else:
+                newValue = float(difference / int(newValue))
+            resolutionBandwidth.delete(0, 'end')
+            resolutionBandwidth.insert(0,str(newValue))
+    return 1
+    
+
 #create root window
 root = Tk()
 root.title("A.R.F. Interface") 
@@ -45,29 +95,35 @@ Label(title_frame, text = 'Port Mode:', font = ('TkDefaultFont', 20)).pack(side 
 
 # Create graph frame
 graph_frame = Frame(root)
-graph_frame.pack(side = TOP)#.grid(column = 0, columnspan = 1, row = 1)
-dataPlot = Canvas(graph_frame, width = 896, height = 509, bg = "black")
-dataPlot.pack(side = LEFT, padx = 10, pady = 10)#grid(column = 0, row = 0, sticky = (W,N), padx = 10, pady = 10)
+graph_frame.pack(side = TOP, fill = BOTH, expand = True)#.grid(column = 0, columnspan = 1, row = 1)
+dataPlot = Canvas(graph_frame, bg = "black")
+dataPlot.pack(side = LEFT, padx = 10, pady = 10, fill = BOTH, expand = True)#grid(column = 0, row = 0, sticky = (W,N), padx = 10, pady = 10)
 
 # Create Settings frame
 settings_frame = Frame(graph_frame)
 settings_frame.pack(side = RIGHT, padx = 20, pady = 11, anchor = N)#grid(column = 1, row = 0, sticky = N, padx = 20, pady = 11)
 Label(settings_frame, text = 'X - Axis Limits ').grid(row = 0, column = 0, pady = 7)
-Entry(settings_frame, width = 5).grid(row = 0, column = 1, pady = 7)
+lowerXLimit = Entry(settings_frame, width = 5, validate = 'focusout', validatecommand = checkBounds)
+lowerXLimit.grid(row = 0, column = 1, pady = 7)
 Label(settings_frame, text = 'Mhz  to ').grid(row = 0, column = 2, pady = 7)
-Entry(settings_frame, width = 5).grid(row = 0, column = 3, pady = 7)
+upperXLimit = Entry(settings_frame, width = 5, validate = 'focusout', validatecommand = checkBounds)
+upperXLimit.grid(row = 0, column = 3, pady = 7)
 Label(settings_frame, text = 'Mhz').grid(row = 0, column = 4, pady = 7)
 Label(settings_frame, text = 'Number of Frequency points ').grid(row = 1, column = 0, \
     columnspan = 3, pady = 7, sticky = W)
 Entry(settings_frame, width = 5).grid(row = 1, column = 3, pady = 7)
 Label(settings_frame, text='Resolution Bandwidth ').grid(row = 2, column = 0, columnspan = 3, \
     pady = 7, sticky = W)
-Entry(settings_frame, width = 5).grid(row = 2, column = 2, pady = 7)
-Label(settings_frame, text = 'khz').grid(row = 2, column = 3, pady = 7, sticky = W)
+resolutionBandwidth = Entry(settings_frame, width = 16, validate = 'focusout', validatecommand = checkResolutionBandwidth)
+resolutionBandwidth.xview_scroll(2, UNITS)
+resolutionBandwidth.grid(row = 2, column = 1, columnspan = 3, pady = 7, sticky = E)
+Label(settings_frame, text = 'khz').grid(row = 2, column = 4, pady = 7)
 Label(settings_frame, text='Output Power ').grid(row = 3, column = 0, pady = 7, sticky = W)
 Entry(settings_frame, width = 5).grid(row = 3, column = 1, pady = 7)
 Label(settings_frame, text = 'dbm').grid(row = 3, column = 2, pady = 7, sticky = W)
-Label(settings_frame, text = 'tx_num').grid(row = 4, column = 0, pady = 7, sticky = W)
+Label(settings_frame, text = 'Transmit Port').grid(row = 4, column = 0, pady = 7, sticky = W)
+txPort = Spinbox(settings_frame, value = [1,2,3,4,5,6], wrap = True, width = 4)
+txPort.grid(row = 4, column = 1, pady = 7)
 Label(settings_frame, text = 'Recording Mode ').grid(row = 5, column = 0, pady = 7, sticky = W)
 recordingMode = IntVar()
 Radiobutton(settings_frame, text = '1 Port Mode', variable = recordingMode, \
